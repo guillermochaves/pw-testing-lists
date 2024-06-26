50304b8225efc27f70fba14f71e99a35fd2e0aea
Guillermo Chaves <guillermochavesg@gmail.com>
2024-06-26 11:21:44 -0600

    new test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,28 +1,43 @@
 import { test, expect } from "@playwright/test";
 
 const TODO_ITEMS: string[] = ["buy some cheese", "feed the cat"];
-test("Add new todo", async ({ page }) => {
-  await page.goto("/");
+// test("Add new todo", async ({ page }) => {
+//   await page.goto("/");
 
-  const newTodo = page.getByPlaceholder("What needs to be done?");
+//   const newTodo = page.getByPlaceholder("What needs to be done?");
 
-  await newTodo.fill(TODO_ITEMS[0]);
-  await newTodo.press("Enter");
+//   await newTodo.fill(TODO_ITEMS[0]);
+//   await newTodo.press("Enter");
 
-  await expect(page.locator('.todo-list')).toContainText([
-    TODO_ITEMS[0]
-  ]);
-});
+//   await expect(page.locator('.todo-list')).toContainText([
+//     TODO_ITEMS[0]
+//   ]);
+// });
 
-test('Validate page title', async ( { page } ) => {
-  await page.goto("/");
+// test('Validate page title', async ( { page } ) => {
+//   await page.goto("/");
 
-  await expect(page.getByRole("heading", { name: "todos" })).toBeVisible();
-});
+//   await expect(page.getByRole("heading", { name: "todos" })).toBeVisible();
+// });
 
 test('Validate google page', async ({page}) => {
   await page.goto('https://www.google.com')
 
   await expect(page.locator('[title="Search"]')).toBeVisible()
+
+});
+
+test('Validate google search', async ({page}) => {
+  await page.goto('https://www.google.com')
+
+  await expect(page.locator('[title="Search"]')).toBeVisible()
+
+  await page.locator('[title="Search"]').fill('vaca')
+
+  await page.locator('[title="Search"]').press('Enter')
+
+  await expect(page.locator('[value="vaca"]')).toBeVisible()
+
+
 
 });

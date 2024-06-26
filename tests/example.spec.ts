ff25ecbc8f4ffc9b5db376caf0ade8512c1d2701
Guillermo Chaves <guillermochavesg@gmail.com>
2024-06-25 22:22:32 -0600

    first commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+const TODO_ITEMS: string[] = ["buy some cheese", "feed the cat"];
+test("Add new todo", async ({ page }) => {
+  await page.goto("/");
+
+  const newTodo = page.getByPlaceholder("What needs to be done?");
+
+  await newTodo.fill(TODO_ITEMS[0]);
+  await newTodo.press("Enter");
+
+  await expect(page.locator('.todo-list')).toContainText([
+    TODO_ITEMS[0]
+  ]);
+});
+
+test('Validate page title', async ( { page } ) => {
+  await page.goto("/");
+
+  await expect(page.getByRole("heading", { name: "todos" })).toBeVisible();
+});
